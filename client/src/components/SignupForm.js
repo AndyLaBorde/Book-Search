0ca7b3cfecee515fc49af2b349fc67f49784309b
Andy LaBorde <32881770+AndyLaBorde@users.noreply.github.com>
2023-01-16 11:45:56 -0700

    updated signup to use mutations with apollo

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { useMutation} from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
-// Replace the addUser() functionality imported from the API file with the ADD_USER mutation functionality.
+// Replace the createUser() functionality imported from the API file with the ADD_USER mutation functionality.
 
 const SignupForm = () => {
   // set initial form state
@@ -30,7 +31,7 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await useMutation(ADD_USER);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
